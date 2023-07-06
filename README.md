ccf4f10c619d672395edae759baf81a77c42ac10
shawnpinciara <shawnpinciara@gmail.com>
2023-07-06 14:29:17 +0200

    update

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # BookletBinderPy IS NOT READY RIGHT NOW
-Pithon tools to convert pdf into ready-to-print booklet to bind into various dispositions
+Python tools to convert pdf into ready-to-print booklet to bind into various dispositions.
+Based on the libraries:
+- pdf2image
+- Pillow
 
-Steps:
-- [convertire pdf in immagini](https://www.geeksforgeeks.org/convert-pdf-to-image-using-python/)
-- selezionare il tipo di booklet
-- [usare ffmpeg per mettere insieme le immagini](https://stackoverflow.com/questions/24604689/how-to-join-two-images-into-one-with-ffmpeg)
--   usare [ffmpeg python](https://github.com/kkroening/ffmpeg-python)
--   usare [filtri ffmpeg](https://ffmpeg.org/ffmpeg-filters.html#hstack)
-- Esportare immagini in pdf nuovo da stampare
+## Working:
+Right now it can just convert an A4 PDF into a new pdf with *two pages per side* 
--- main.py
+++ main.py
@@ -1,48 +1,41 @@
 # import module
 from pdf2image import convert_from_path
-#import ffmpeg
 import math
 from PIL import Image
 import os
+
 #pdf name
-pdf_name = 'test.pdf'
+pdf_name = 'DIY.pdf'
 
-# from pdf to images
-images = convert_from_path(pdf_name)
-images = [image.rotate(-90,expand=True) for image in images]
-images_concat = []
-#creare dei pattern di riarrangiamento delle immagini
-print(len(images))
-for i in range(len(images)-1):
-    #TODO: sistemare in modo che vada di due in due
-    print(i)
-    im1 = images[i]
-    im2 = images[i+1]
-    dst = Image.new('RGB', (im1.width, im1.height * 2))
-    dst.paste(im1, (0, 0))
-    dst.paste(im2, (0, im1.height))
+def a4_to_2_a5():
+    images = convert_from_path(pdf_path=pdf_name,first_page=111+17,last_page=141+17)
+    images = [image.rotate(-90,expand=True) for image in images]
+    images_concat = []
+    images_concat_inverse = []
+    for i in range(len(images)-1):
+        #TODO: sistemare in modo che vada di due in due
+        if i%2==0:
+            #print(i)
+            im1 = images[i]
+            im2 = images[i+1]
+            dst = Image.new('RGB', (im1.width, im1.height * 2))
+            dst.paste(im1, (0, 0))
+            dst.paste(im2, (0, im1.height))
+            dst.resize((595, 842))
+            images_concat.append(dst)
+
+    #reverse option
+    imsave = Image.new('RGB', (im1.width, im1.height * 2))
     images_concat.append(dst)
-    # dst.save(str(i)+'concat.jpg')
-    # images[i].save('images_from_pdf/im'+ str(i) +'.jpg', 'JPEG')
-
-# #images[0].save('im0.jpg', 'JPEG')
-# #images[1].save('im1.jpg', 'JPEG')
-# im1 = Image.open('im0.jpg').rotate(-90,expand=True).save('im1_r.jpg')
-# im2 = Image.open('im1.jpg').rotate(-90,expand=True).save('im2_r.jpg')
-# im1 = Image.open('im1_r.jpg')
-# im2 = Image.open('im2_r.jpg')
-
-# def get_concat_v(im1, im2):
-#     dst = Image.new('RGB', (im1.width, im1.height * 2))
-#     dst.paste(im1, (0, 0))
-#     dst.paste(im2, (0, im1.height))
-#     return dst
-
-# get_concat_v(im1, im2).save('concat_v.jpg')
-# #im1 = ffmpeg.input(images[0])
-# #im2 = ffmpeg.input(images[1])
-# #ffmpeg.filter([im1,im2],"scale=120:-1,tile=2x1").output("out.jpg").run()
-
-# def a4_to_2_a5():
+    print(len(images_concat))
+    for i in range((int(len(images_concat)/2)) -1):
+        images_concat_inverse.append(images_concat[i])
+        images_concat_inverse.append(images_concat[len(images_concat)-i-1].rotate(180,expand=True))
+    images_concat = images_concat_inverse
+    #END OF REVERSE OPTION
     
-#     print('ok')
+    imsave = Image.new('RGB', (im1.width, im1.height * 2))
+    imsave.save('concat.pdf', save_all=True, append_images=images_concat)
+
+
+a4_to_2_a5()
